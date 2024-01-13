82129af4f3c7ac053e0b17a3cb38b6c3135345c2
Vegard Løwe <vegardlowejensen@vegard-sin-mbp.lan>
2024-01-13 11:45:05 +0100

    added solar data

--- README.md
+++ README.md
@@ -11,3 +11,75 @@ yarn install
 ```sh
 yarn dev
 ```
+
+## Introduction
+
+This project uses Mapbox GL for creating and styling maps. Here are some basic guides on how to add and style layers in Mapbox GL.
+
+### How to add a source in Mapbox GL
+
+Sources in Mapbox GL are used to specify the data that will be used in your map. You can add a source to your map by using the `addSource` method of the `map` object:
+
+```javascript
+map.addSource("my-source", {
+  type: "geojson",
+  data: {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-77.0323, 38.9131],
+    },
+    properties: {
+      title: "Mapbox DC",
+      "marker-symbol": "monument",
+    },
+  },
+});
+```
+
+This will add a GeoJSON source with the id my-source to the map. The source contains a single point feature located in Washington, D.C.
+
+For more details, refer to the official Mapbox GL documentation: https://docs.mapbox.com/mapbox-gl-js/api/map/#map#addsource
+
+### How to add layers in Mapbox GL
+
+Layers in Mapbox GL are used to specify how data should be drawn on the map. Each layer needs a source which specifies the data that the layer will render. You can add a layer to your map by using the `addLayer` method of the `map` object:
+
+```javascript
+map.addLayer({
+  id: "my-layer",
+  type: "fill",
+  source: "my-source",
+  layout: {},
+  paint: {
+    "fill-color": "#6a0dad",
+    "fill-opacity": 0.75,
+  },
+});
+```
+
+For more details, refer to the official Mapbox GL documentation: https://docs.mapbox.com/mapbox-gl-js/api/map/#map#addlayer
+
+### How to style layers in Mapbox GL
+
+You can style layers in Mapbox GL by using the setPaintProperty method of the map object:
+
+```javascript
+map.setPaintProperty("my-layer", "fill-color", "#ff0000");
+```
+
+This will change the fill color of the layer with the id my-layer to red.
+
+For more details, refer to the official Mapbox GL documentation: https://docs.mapbox.com/mapbox-gl-js/api/map/#map#setpaintproperty
+
+### How to remove layers in Mapbox GL
+
+You can remove a layer from your map by using the removeLayer method of the map object:
+
+```javascript
+map.removeLayer("my-layer");
+```
+
+This will remove the layer with the id my-layer from the map.
+
+For more details, refer to the official Mapbox GL documentation: https://docs.mapbox.com/mapbox-gl-js/api/map/#map#removelayer
--- components/Map.tsx
+++ components/Map.tsx
@@ -1,49 +1,80 @@
-import React, { useEffect, useRef } from "react";
-import mapboxgl from "mapbox-gl";
-import { data } from "../data/Stores";
+import React, { MutableRefObject, useCallback, useEffect, useRef } from "react";
+import Head from "next/head";
+import mapboxgl, { Map as MapboxMap } from "mapbox-gl";
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN as string;
 
 const Map = () => {
   const mapContainerRef = useRef(null);
+  const map: MutableRefObject<null | MapboxMap> = useRef(null);
 
   useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [0, 50],
-      zoom: 4,
-    });
-
-    map.on("load", () => {
-      // Add a data source containing GeoJSON data.
-      map.addSource("stores", {
-        type: "geojson",
-        data: data,
+    if (mapContainerRef.current) {
+      map.current = new mapboxgl.Map({
+        container: mapContainerRef.current,
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [-120, 35],
+        zoom: 7,
       });
 
-      // Add a new layer to visualize the polygon.
-      map.addLayer({
-        id: "stores",
-        type: "circle",
-        source: "stores", // reference the data source
-        layout: {},
-        paint: {
-          "circle-color": "#0080ff", // blue color fill
-          "circle-opacity": 0.5,
-          "circle-stroke-color": "#fff",
-          "circle-stroke-width": 1,
-        },
-      });
-    });
+      map.current.on("load", () => {
+        // Add a data source containing GeoJSON data.
+        map.current!.addSource("solar", {
+          type: "geojson",
+          data: "/data/Solar_footprints.geojson",
+        });
 
-    return () => {
-      map.remove();
-    };
+        // Add a new layer to visualize the polygon.
+        map.current!.addLayer({
+          id: "solar_footprints",
+          type: "fill",
+          source: "solar", // reference the data source
+          layout: {},
+          paint: {
+            "fill-color": "#0080ff", // blue color fill
+            "fill-opacity": 0.5,
+          },
+        });
+
+        map.current!.addControl(new mapboxgl.NavigationControl());
+
+        map.current!.on("click", "solar_footprints", clickFeatures);
+      });
+
+      return () => {
+        map.current!.remove();
+      };
+    }
+  }, []);
+
+  const clickFeatures = useCallback((e) => {
+    if (e.features) {
+      const coordinates = e.features[0].geometry.coordinates[0][0];
+      const county = e.features[0].properties.COUNTYNAME;
+      const size = e.features[0].properties.Acres;
+      const type = e.features[0].properties.type;
+
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+
+      new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setHTML(
+          `<h3>${county}</h3><p>${size.toFixed(2)} acres</p><p>${type}</p>`
+        )
+        .addTo(map.current!);
+    }
   }, []);
 
   return (
     <div>
+      <Head>
+        <link
+          href="https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.css"
+          rel="stylesheet"
+        />
+      </Head>
       <div
         ref={mapContainerRef}
         style={{ width: "100%", height: "100vh" }}
